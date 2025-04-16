89a427a33dd18e5c370ba8e1e18fc3740bad9122
pwflamy <bestprogi@gmail.com>
2025-04-17 01:06:34 +0200

    New tasks for enum and chaining methods

---
+++ com/pwflamy/tasks/chainmethods/Person.java
@@ -0,0 +1,14 @@
+package FlamyMap.com.pwflamy.tasks.chainmethods;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Прочитать, что такое "Chaining method" в java. Добавить в этот класс сеттеры с использованием этого стиля программирования.
+ * Создать метод main, в котором создать несколько объектов Person и заполнить им поля, используя созданные сеттеры
+ */
+public class Person {
+
+    private Long id;
+    private String name;
+    private ZonedDateTime createdAt;
+}
